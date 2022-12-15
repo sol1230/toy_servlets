c30f04d80f09dcd5c7c6fe9df6552a87540bec10
sol1230 <solllll1230@gmail.com>
2022-12-15 09:30:07 +0900

    first commit

---
+++ src/test/java/com/sol1230/toy_servlets/ToyServletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sol1230.toy_servlets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyServletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
